5777fd93e559032617d721fb2db4a933de2eff44
Leandro <leandrodasilvalara@yahoo.com.br>
2022-03-03 23:34:45 -0300

    Criando Activity de Depositar

---
+++ app/src/main/java/com/br/leandro/bancodigital/deposito/DepositoFormActivity.java
@@ -0,0 +1,16 @@
+package com.br.leandro.bancodigital.deposito;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.br.leandro.bancodigital.R;
+
+public class DepositoFormActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_deposito_form);
+    }
+}
